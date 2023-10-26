1837f7f8463c8d3caf9fb6c5469c7dc6fcb4c76a
Emma Skoglund <emma_skoglund@outlook.com>
2023-10-26 15:09:35 +0200

    info om uppgiften

---
+++ application/app.py
@@ -0,0 +1,7 @@
+""" Webbaplikationen ska inehålla 
+test-case
+cookies 
+mins ett externt, gärna fler
+Visa data i strukturerad form, gärna i tabellform med Pandas och kanske skicka någon
+Pandas-kolumn till Plotly för att illustrera antal böcker eller annat som kan beräknas i ett diagram.
+"""
