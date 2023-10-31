812c9a37ee67635acc45f4b9ec083998afb64fbc
Edvin Jäderlund <edvin.jaderlund@gmail.com>
2023-10-31 14:03:54 +0100

    Rollback app.py
    app.py rollbacked

--- application/app.py
+++ application/app.py
@@ -1,27 +1,33 @@
 from flask import Flask, render_template, request
-import requests
 import pycountry
+import requests
 
 app = Flask(__name__)
+
 
 def get_country_city_data():
     api_url = 'http://api.citybik.es/v2/networks'
     response = requests.get(api_url)
 
+    country_names = set()
+    cities = set()
+
     if response.status_code == 200:
         data = response.json()
         networks = data.get('networks', [])
-        countries = set()
-        cities = set()
 
         for network in networks:
             location = network.get('location', {})
-            countries.add(location.get('country', ''))
+            country_code = location.get('country', '')
+            country = pycountry.countries.get(alpha_2=country_code)
+            country_name = country.name if country else 'Unknown Country'
+            country_names.add(country_name)
             cities.add(location.get('city', ''))
 
-        return sorted(list(countries)), sorted(list(cities))
+        return sorted(country_names), sorted(list(cities))
 
     return [], []
+
 
 def get_stations_by_country(country_code):
     api_url = 'http://api.citybik.es/v2/networks'
@@ -32,8 +38,6 @@ def get_stations_by_country(country_code):
         networks = data.get('networks', [])
         stations = []
 
-
-
         for network in networks:
             location = network.get('location', {})
             if location.get('country') == country_code:
@@ -41,26 +45,23 @@ def get_stations_by_country(country_code):
 
         return stations
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    countries, cities = get_country_city_data()
+    country_names, cities = get_country_city_data()
 
     stations = []
 
     if request.method == 'POST':
-        country_code = request.form['country']
-        if country_code == 'all':
+        country_name = request.form['country']
+        if country_name == 'All Countries':
             stations = get_stations_by_country('')
         else:
+            country_code = pycountry.countries.lookup(country_name).alpha_2
             stations = get_stations_by_country(country_code)
 
-    return render_template('index.html', stations=stations, countries=countries, cities=cities)
+    return render_template('index.html', stations=stations, countries=country_names, cities=cities)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-#https://www.google.com/maps/embed/v1/view
- # ?key=YOUR_API_KEY
- # &center=-33.8569,151.2152
-# &zoom=18
- # &maptype=satellite
